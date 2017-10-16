3ec3a9e217b3fa9ea03eff5bdf91e3ba15605504
cc <313420862@qq.com>
2017-10-15 23:30:16 -0700

    带上foreign key

--- todolist/models.py
+++ todolist/models.py
@@ -5,12 +5,17 @@ from django.db import models
 
 
 # # Create your models here.
+from account.models import Account
+
+
 class Todo(models.Model):
     title = models.CharField('todo标题', max_length=100)
     todo_time = models.DateTimeField(blank=False)
     note = models.CharField('备注', max_length=200, blank=True, default='')
     sms_remid = models.BooleanField('短信提醒', default=False)
     created = models.DateTimeField(auto_now_add=True)
+    # related_name 用于反向查询
+    account = models.ForeignKey(Account, related_name='strategy')
 
     class Meta:
         ordering = ('created',)
